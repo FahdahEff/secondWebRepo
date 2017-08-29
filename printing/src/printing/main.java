e02cc3aa70e77f2cf5b355cedf5747c499221109
FahdahEff <fahdah1996@gmail.com>
2017-08-29 05:31:31 +0300

    second print

--- printing/src/printing/main.java
+++ printing/src/printing/main.java
@@ -3,5 +3,7 @@ package printing;
 public class main {
 public static void main(String[]args) {
 	System.out.println("Let's try again.");
+	System.out.println("second.");
+	System.out.println("second.");
 }
 }
